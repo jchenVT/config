0b59011baa28bdd44cb069b6b453f09d1b5a4db0
a646273 <a646273@vc2crtp1400463n.fmr.com>
2019-06-04 10:12:57 -0400

    added alias to spinup

--- spinup.sh
+++ spinup.sh
@@ -1,8 +1,11 @@
 git clone https://github.com/jchenVT/config.git
 rm ~/.vimrc
 rm ~/.tmux.conf
+ln -s /home/a646273/config/bashaliaswork /home/a646273/.bash_aliases
 ln -s /home/a646273/config/vimrcwork /home/a646273/.vimrc
 ln -s /home/a646273/config/tmuxwork /home/a646273/.tmux.conf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir .vim/backup
+mkdir .vim/tmp
 vim -c "PlugInstall"

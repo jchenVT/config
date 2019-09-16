3db93445c587df120cbc8a8d6d55cee7340e9d5c
jchenvt <jchen20@vt.edu>
2019-09-16 16:22:38 -0400

    added whoami

--- spinup.sh
+++ spinup.sh
@@ -1,8 +1,8 @@
 rm ~/.vimrc
 rm ~/.tmux.conf
-ln -s /home/a646273/config/bashaliaswork /home/a646273/.bash_aliases
-ln -s /home/a646273/config/vimrcwork /home/a646273/.vimrc
-ln -s /home/a646273/config/tmuxwork /home/a646273/.tmux.conf
+ln -s /home/$(whoami)/config/bashaliaswork /home/$(whoami)/.bash_aliases
+ln -s /home/$(whoami)/config/vimrc /home/$(whoami)/.vimrc
+ln -s /home/$(whoami)/config/tmuxw /home/$(whoami)/.tmux.conf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.vim/backup
